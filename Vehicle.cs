2536e9775d11a50af0f84a31f3e65ae5bfadd701
Mayank Sharma <Mayanksharma247@gmail.com>
2024-03-27 19:39:02 -0500

    Add project files.

---
+++ Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_new_Assignment3_mayank
+{
+    internal class Vehicle
+    {
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public int Year { get; set; }
+        public double RentalPrice { get; set; }
+
+
+
+        public Vehicle(string model, string manufacturer, int year, double rentalPrice)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Year = year;
+            RentalPrice = rentalPrice;
+        }
+
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"Model- {Model}");
+            Console.WriteLine($"Manufacturer- {Manufacturer}");
+            Console.WriteLine($"Year- {Year}");
+            Console.WriteLine($"Rental Price- {RentalPrice:C}");
+        }
+    }
+}
