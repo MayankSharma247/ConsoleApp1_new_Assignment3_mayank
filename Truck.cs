eb43107cbd4d6406689c59f956d20b203b445335
Mayank Sharma <Mayanksharma247@gmail.com>
2024-03-27 19:45:38 -0500

    Created truck class

---
+++ Truck.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_new_Assignment3_mayank
+{
+    internal class Truck: Vehicle
+    {
+        private double capacity;
+        private string truckType;
+        private bool fourWheelDrive;
+
+
+        public Truck(string model, string manufacturer, int year, double rentalPrice, double Capacity, string TruckType, bool FourWheelDrive) : base(model, manufacturer, year, rentalPrice)
+        {
+
+            capacity = Capacity;
+            truckType = TruckType;
+            fourWheelDrive = FourWheelDrive;
+
+        }
+
+
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Capacity- {capacity} tons");
+            Console.WriteLine($"Truck Type- {truckType}");
+
+            if (fourWheelDrive == true)
+            {
+                Console.WriteLine("Four Wheel Drive- Yes");
+            }
+            else
+            {
+                Console.WriteLine("Four Wheel Drive- No");
+            }
+
+        }
+
+
+    }
+}
